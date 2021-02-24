ea8abd93771a92e7d503fc0f47a759594c347514
Logan Morro <loganfmorro@gmail.com>
2021-02-24 18:43:56 -0500

    built main components

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="m-10 text-sm text-center text-black-500">
+            <p>Employee pictures provided by <a href="https://randomuser.me/">Randomuser.Me</a></p>
+            <p>Built by <a href="https://canahuate16.github.io/">Logan Morro</a></p>
+        </div>
+    )
+}
+
+export default Footer;
