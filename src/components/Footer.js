56ee34db6907d8cb783a73b303940fdab7d359dc
Logan Morro <loganfmorro@gmail.com>
2021-02-24 19:41:41 -0500

    trying to configure npm packages

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ function Footer() {
     return (
         <div className="m-10 text-sm text-center text-black-500">
             <p>Employee pictures provided by <a href="https://randomuser.me/">Randomuser.Me</a></p>
-            <p>Built by <a href="https://canahuate16.github.io/">Logan Morro</a></p>
+            <p>Built by <a href="https://loganfmorro.github.io/">Logan Morro</a></p>
         </div>
     )
 }
