03f50a174a9b5e94a838611600c02af21d26ab8e
Logan Morro <loganfmorro@gmail.com>
2021-03-01 23:29:43 -0500

    updating npm package

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import Layout from "./components/Layout";
 import Header from "./components/Header";
 import Navigation from "./components/Navigation";
-import EmployeeCardList from "./components/employeeCardList";
+import EmployeeCardList from "./components/EmployeeCardList";
 import Footer from "./components/Footer";
+import employees from "./data/employees.json";
 
 function App() {
     const [ searchTerm, setSearchTerm ] = useState("");
