4db1bdab99353ca7239c22cff6b5a28085e1e592
Logan Morro <loganfmorro@gmail.com>
2021-03-01 23:17:00 -0500

    Updates

--- src/utils/API.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
-
-
-export default {
-  searchTerms: function() {
-    return axios.get(BASEURL);
-  }
-};
